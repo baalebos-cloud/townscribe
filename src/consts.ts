521439040600e7292db810dab6adde057cb273c6
Oluwadare T. Jayeola <69169679+baalebos-cloud@users.noreply.github.com>
2026-06-02 10:48:09 +0100

    Change SITE_TITLE from 'Townscribe Blog' to 'Townscribe Media'

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Townscribe Blog';
+export const SITE_TITLE = 'Townscribe Media';
 export const SITE_DESCRIPTION = 'Welcome to Townscribe!';
